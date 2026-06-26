44a967eadb29b76248a7d37ebeccf4b3ff7b625b
Sergey Goryachev <spld@yandex.ru>
2026-06-26 12:35:52 +0300

    add services

--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -1,89 +1,32 @@
 package ru.yandex.practicum.catsgram.controller;
 
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
-import ru.yandex.practicum.catsgram.exception.DuplicatedDataException;
-import ru.yandex.practicum.catsgram.exception.NotFoundException;
 import ru.yandex.practicum.catsgram.model.User;
+import ru.yandex.practicum.catsgram.service.UserService;
 
-import java.time.Instant;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private final Map<Long, User> users = new HashMap<>();
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
     public Collection<User> getAllUsers() {
-        return users.values();
+        return userService.getAllUsers();
     }
 
     @PostMapping
     public User create(@RequestBody User user) {
-        checkUserRequiredFields(user);
-        checkDuplicateEmail(user);
-
-        user.setId(getNextId());
-        user.setRegistrationDate(Instant.now());
-
-        users.put(user.getId(), user);
-
-        return user;
+        return userService.create(user);
     }
 
     @PutMapping
     public User update(@RequestBody User newUser) {
-        if (newUser.getId() == null) {
-            throw new ConditionsNotMetException("Id должен быть указан");
-        }
-
-        checkDuplicateEmail(newUser);
-
-        if (users.containsKey(newUser.getId())) {
-            User oldUser = users.get(newUser.getId());
-
-            checkUserRequiredFields(newUser);
-
-            oldUser.setEmail(newUser.getEmail());
-            oldUser.setUsername(newUser.getUsername());
-            oldUser.setPassword(newUser.getPassword());
-
-            return oldUser;
-        }
-
-        throw new NotFoundException("Пользователь с id = " + newUser.getId() + " не найден");
-    }
-
-    private void checkUserRequiredFields(User user) {
-        if (user.getEmail() == null || user.getEmail().isBlank()) {
-            throw new ConditionsNotMetException("Email не может быть пустым");
-        }
-
-        // TODO: добавить проверку имени, закоментировано для прохождения тестов
-//        if (user.getUsername() == null || user.getUsername().isBlank()) {
-//            throw new ConditionsNotMetException("Имя не может быть пустым");
-//        }
-
-        if (user.getPassword() == null || user.getPassword().isBlank()) {
-            throw new ConditionsNotMetException("Пароль не может быть пустым");
-        }
-    }
-
-    private void checkDuplicateEmail(User user) {
-        if (users.values().stream().anyMatch(u -> user.getEmail().equals(u.getEmail()))) {
-            throw new DuplicatedDataException("Этот имейл уже используется");
-        }
-    }
-
-    private long getNextId() {
-        long currentMaxId = users.keySet()
-                .stream()
-                .mapToLong(id -> id)
-                .max()
-                .orElse(0);
-        return ++currentMaxId;
+        return userService.update(newUser);
     }
 }
